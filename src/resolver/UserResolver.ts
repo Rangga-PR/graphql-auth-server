339c9a4aa5c0a9b92e8e4cd14297a641a8ecd57b
Rangga Putra Raharja <rangga-pr@pop-os.localdomain>
2020-02-26 07:31:19 +0700

    hash password using bcrypt

--- src/resolver/UserResolver.ts
+++ src/resolver/UserResolver.ts
@@ -1,5 +1,6 @@
 import { Resolver, Mutation, InputType, Field, Arg, Query } from "type-graphql";
 import { User } from "../entity/User";
+import { hash } from "bcryptjs";
 
 @InputType()
 class RegisterInput {
@@ -26,6 +27,8 @@ class RegisterInput {
 export class UserResolver {
   @Mutation(() => User)
   async register(@Arg("user", () => RegisterInput) user: RegisterInput) {
+    const hashedPassword = hash(user.password, 12);
+    user.password = await hashedPassword;
     const registeredUser = await User.create(user).save();
     return registeredUser;
   }
